2bd900092f73000ac101ab745f60416dc40b69b1
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-07-02 19:35:56 -0600

    Fix file paths, now link in vim config files too, update git config

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/shell/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/vim/.viminfo" ~
 
 if [ "$(uname)" == "Darwin" ]; then
   if [ -f "$DOTFILES_DIR/install/osx.sh" ]; then
